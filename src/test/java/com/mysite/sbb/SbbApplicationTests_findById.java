f05f26c7dfec8695d6571d1bc100462bb8421e50
양지훈 (차장) (주)더이룸소프트 (yanghohu@cnspartner.com <yanghohu@CNS.LGCNS.COM>
2025-02-17 13:45:03 +0900

    6.findById add

---
+++ src/test/java/com/mysite/sbb/SbbApplicationTests_findById.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SbbApplicationTests_findById {
+
+
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Test
+	void testJPA() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if(oq.isPresent()) {
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
+	}
+
+}

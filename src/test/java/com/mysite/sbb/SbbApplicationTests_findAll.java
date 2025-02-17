492adcf4a1dbef506cf19a2350bdea490b0673bb
양지훈 (차장) (주)더이룸소프트 (yanghohu@cnspartner.com <yanghohu@CNS.LGCNS.COM>
2025-02-17 13:28:31 +0900

    5.findAll test

---
+++ src/test/java/com/mysite/sbb/SbbApplicationTests_findAll.java
@@ -0,0 +1,29 @@
+package com.mysite.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SbbApplicationTests_findAll {
+
+
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Test
+	void testJPA() {
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		System.out.println(q.getSubject());
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+}

d8a5d616e444c5276af4d6a299d838170a219c49
양지훈 (차장) (주)더이룸소프트 (yanghohu@cnspartner.com <yanghohu@CNS.LGCNS.COM>
2025-02-17 16:51:15 +0900

    11.service -> Repository 호출로 변경

--- src/main/java/com/mysite/sbb/question/QestionController.java
+++ src/main/java/com/mysite/sbb/question/QestionController.java
@@ -12,12 +12,12 @@ import java.util.List;
 @Controller
 public class QestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     //@ResponseBody
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList",questionList);
         return "question_list";
     }

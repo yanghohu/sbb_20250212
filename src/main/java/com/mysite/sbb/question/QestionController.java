2c645b24743b090021bbd1ade09e53f67690fea1
양지훈 (차장) (주)더이룸소프트 (yanghohu@cnspartner.com <yanghohu@CNS.LGCNS.COM>
2025-02-18 10:52:05 +0900

    12.Controller getService add

--- src/main/java/com/mysite/sbb/question/QestionController.java
+++ src/main/java/com/mysite/sbb/question/QestionController.java
@@ -1,9 +1,11 @@
 package com.mysite.sbb.question;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -20,5 +22,12 @@ public class QestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList",questionList);
         return "question_list";
+    }
+
+    @GetMapping("/question/detail/{id}")
+    public String detail(Model model,@PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question",question);
+        return "question_detail";
     }
 }

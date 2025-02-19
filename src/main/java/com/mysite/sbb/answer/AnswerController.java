e23cf2061786fec7b57dd8b792b5fab0d7ee72da
양지훈 (차장) (주)더이룸소프트 (yanghohu@cnspartner.com <yanghohu@CNS.LGCNS.COM>
2025-02-19 14:24:11 +0900

    14.답변오류 해결

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -20,7 +20,7 @@ public class AnswerController {
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id,
-                               @RequestParam String content) {
+                               @RequestParam(value = "content") String content) {
         Question question = this.questionService.getQuestion(id);
         this.answerService.create(question, content);
         return String.format("redirect:/question/detail/%s", id);

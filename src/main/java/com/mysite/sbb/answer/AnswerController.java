b08447396e2f4c314198d1d0832ae17b581dedcc
양지훈 (차장) (주)더이룸소프트 (yanghohu@cnspartner.com <yanghohu@CNS.LGCNS.COM>
2025-02-19 10:32:17 +0900

    14.답변글 추가

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,7 +2,6 @@ package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,12 +16,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AnswerController {
 
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String dreateAnswer(Model model, @PathVariable("id") Integer id,
+    public String createAnswer(Model model, @PathVariable("id") Integer id,
                                @RequestParam String content) {
         Question question = this.questionService.getQuestion(id);
-        // TODO : 답변을 등록한다.
+        this.answerService.create(question, content);
         return String.format("redirect:/question/detail/%s", id);
 
     }
